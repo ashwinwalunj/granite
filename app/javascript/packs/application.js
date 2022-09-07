895933792442827119f8b84777de58f3186aba6c
ashwinwalunj <ashwinwalunj@gmail.com>
2022-09-07 21:09:35 +0530

    Added reusable components

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,5 @@ require("channels");
 var componentRequireContext = require.context("src", true);
 var ReactRailsUJS = require("react_ujs");
 
+// eslint-disable-next-line react-hooks/rules-of-hooks
 ReactRailsUJS.useContext(componentRequireContext);
