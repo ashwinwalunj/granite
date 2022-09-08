a3dc467b19d172accd6f76024a8b0c61479f47f4
ashwinwalunj <ashwinwalunj@gmail.com>
2022-09-08 11:04:40 +0530

    Added ability to logout

--- app/javascript/src/apis/axios.js
+++ app/javascript/src/apis/axios.js
@@ -1,12 +1,28 @@
 import axios from "axios";
 
 import Toastr from "components/Common/Toastr";
-import { setToLocalStorage } from "utils/storage";
+import { setToLocalStorage, getFromLocalStorage } from "utils/storage";
 
 const DEFAULT_ERROR_NOTIFICATION = "Something went wrong!";
+
 axios.defaults.baseURL = "/";
 
-// Remaining code
+const setAuthHeaders = (setLoading = () => null) => {
+  axios.defaults.headers = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    "X-CSRF-TOKEN": document
+      .querySelector('[name="csrf-token"]')
+      .getAttribute("content"),
+  };
+  const token = getFromLocalStorage("authToken");
+  const email = getFromLocalStorage("authEmail");
+  if (token && email) {
+    axios.defaults.headers["X-Auth-Email"] = email;
+    axios.defaults.headers["X-Auth-Token"] = token;
+  }
+  setLoading(false);
+};
 
 const handleSuccessResponse = response => {
   if (response) {
@@ -39,20 +55,10 @@ const registerIntercepts = () => {
     handleErrorResponse(error)
   );
 };
-export { registerIntercepts };
-export const setAuthHeaders = (setLoading = () => null) => {
-  axios.defaults.headers = {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-    "X-CSRF-TOKEN": document
-      .querySelector('[name="csrf-token"]')
-      .getAttribute("content"),
-  };
-  const token = localStorage.getItem("authToken");
-  const email = localStorage.getItem("authEmail");
-  if (token && email) {
-    axios.defaults.headers["X-Auth-Email"] = email;
-    axios.defaults.headers["X-Auth-Token"] = token;
-  }
-  setLoading(false);
+
+const resetAuthTokens = () => {
+  delete axios.defaults.headers["X-Auth-Email"];
+  delete axios.defaults.headers["X-Auth-Token"];
 };
+
+export { setAuthHeaders, registerIntercepts, resetAuthTokens };
